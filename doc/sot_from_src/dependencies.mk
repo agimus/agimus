a2d0d72a0cbc217ac10411994d87c749bb5b92fc
Joseph Mirabel <jmirabel@laas.fr>
2019-10-08 18:55:27 +0200

    Add dependency between pinocchio and sot-core.

--- doc/sot_from_src/dependencies.mk
+++ doc/sot_from_src/dependencies.mk
@@ -14,7 +14,7 @@ dynamic_graph_bridge.configure.dep: dynamic-graph.install \
 	sot-core.install
 
 ##### SoT #######################################
-sot-core.configure.dep: dynamic-graph-python.install
+sot-core.configure.dep: pinocchio.install dynamic-graph-python.install
 sot-tools.configure.dep: sot-core.install
 sot-dynamic-pinocchio.configure.dep: pinocchio.install \
 	sot-core.install sot-tools.install
