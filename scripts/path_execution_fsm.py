a328e27bb01c78febcc49c0470267f29522a5a2a
Joseph Mirabel <jmirabel@laas.fr>
2019-11-26 11:23:50 +0100

    Fix ROS nodes.

--- scripts/path_execution_fsm.py
+++ scripts/path_execution_fsm.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 from agimus.path_execution import makeStateMachine
-import rospy
+import rospy, smach
 
-rospy.init_node("agimus")
-sm, sis = makeStateMachine()
+def run():
+    rospy.init_node("agimus")
+    sm, sis = makeStateMachine()
 
-sis.start()
-outcome = sm.execute()
-sis.stop()
+    sis.start()
+    outcome = sm.execute()
+    sis.stop()
+
+if __name__ == "__main__":
+    try:
+        run()
+    except rospy.ROSInterruptException:
+        pass
+    except smach.InvalidUserCodeError as e:
+        if "ROSInterruptException" in str(e):
+            raise Exception ("I was able to catch the following exception as a ROSInterruptException\n" + str(e))
+        else:
+            raise e
