b55ef2bb3f0ad45c51ef56b53b79e7b75a9451cc
Joseph Mirabel <jmirabel@laas.fr>
2020-02-05 16:06:19 +0100

    Add SUFFIX_SO_VERSION for packages that support it.

--- doc/sot_from_src/repos.mk
+++ doc/sot_from_src/repos.mk
@@ -7,9 +7,11 @@ pinocchio_extra_flags=-DBUILD_UNIT_TESTS=OFF
 
 dynamic-graph_branch=master
 dynamic-graph_repository=${SOT_REPO}
+dynamic-graph_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 dynamic-graph-python_branch=master
 dynamic-graph-python_repository=${SOT_REPO}
+dynamic-graph-python_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 dynamic_graph_bridge_branch=master
 dynamic_graph_bridge_repository=${SOT_REPO}
@@ -19,18 +21,22 @@ dynamic_graph_bridge_msgs_repository=${SOT_REPO}
 
 sot-core_branch=master
 sot-core_repository=${SOT_REPO}
+sot-core_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 sot-tools_branch=master
 sot-tools_repository=${SOT_REPO}
+sot-tools_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 sot-dynamic-pinocchio_branch=master
 sot-dynamic-pinocchio_repository=${SOT_REPO}
+sot-dynamic-pinocchio_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 roscontrol_sot_branch=master
 roscontrol_sot_repository=${SOT_REPO}
 
 sot-talos_branch=master
 sot-talos_repository=${SOT_REPO}
+sot-talos_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 talos_data_branch=master
 talos_data_repository=${PYRENE_DEV_REPO}
@@ -43,6 +49,7 @@ talos_metapkg_ros_control_sot_subdirs+=talos_metapkg_ros_control_sot
 
 sot-tiago_branch=master
 sot-tiago_repository=${SOT_REPO}
+sot-tiago_extra_flags=-DSUFFIX_SO_VERSION=OFF
 
 tiago_data_branch=master
 tiago_data_repository=${SOT_REPO}
